3502a774b274112bb4956ca98af7cc79e66e7fe1
alimun88 <ayon88@hotmail.com>
2016-12-18 06:22:06 +0000

    Added form validation to modal

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+ validates :first_name, presence: true 
+ validates :last_name, presence: true 
+ validates :email, presence: true 
+ validates :comments, presence: true 
 end
